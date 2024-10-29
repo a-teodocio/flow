8eab9c64b73e7982c9884f9f0449edd6258b0b48
Aivee Teodocio <aiveeteo6@gmail.com>
2024-10-29 16:52:14 -0500

    add styling to prevent words from breaking

--- src/components/WordsContainer.tsx
+++ src/components/WordsContainer.tsx
@@ -1,21 +1,26 @@
 import Words from "./Words";
 import UserTypings from "./UserTypings";
 
-
-const WordsContainer = (
-        { wordsGenerated, userInput } : 
-        { wordsGenerated: string, userInput: string }
-    ) => {
-        return ( 
-        <div className="relative max-w-3xl mt-3 text-3xl leading-relaxed break-all text-primary-600">
-            <Words words={wordsGenerated}/>
-            <UserTypings 
-                className="absolute inset-0"
-                userInput={userInput}
-                words={wordsGenerated}
-            />
-        </div>
-    );
+const WordsContainer = ({
+  wordsGenerated,
+  userInput,
+}: {
+  wordsGenerated: string;
+  userInput: string;
+}) => {
+  return (
+    <div
+      className="relative max-w-3xl mt-3 text-3xl leading-relaxed  text-primary-600 break-all"
+      style={{ whiteSpace: "pre-wrap", wordBreak: "break-word" }}
+    >
+      <Words words={wordsGenerated} />
+      <UserTypings
+        className="absolute inset-0"
+        userInput={userInput}
+        words={wordsGenerated}
+      />
+    </div>
+  );
 };
 
 export default WordsContainer;
