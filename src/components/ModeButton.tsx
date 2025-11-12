023abeb4d9035f7165b1d243f42f1bb111426028
a.teodocio <aiveeteo6@gmail.com>
2025-11-12 01:18:24 -0600

    Fix Timer mode bug causing countdown to reset

--- src/components/ModeButton.tsx
+++ src/components/ModeButton.tsx
@@ -2,11 +2,19 @@ import { useRef } from "react";
 
 const ModeButton = ({ className, text, onClick } : {className?: string, text: string, onClick: () => void}) => {
     const buttonRef = useRef<HTMLButtonElement>(null);
+
+    const handleClick = () => {
+        onClick();
+        if (buttonRef.current) {
+            buttonRef.current.blur();
+        }
+    };
+
     return (
         <button 
             ref={buttonRef} 
             className={className} 
-            onClick={onClick}
+            onClick={handleClick}
         >
             {text}
         </button>
